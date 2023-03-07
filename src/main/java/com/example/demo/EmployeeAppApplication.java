dc894beeb1a9b37da29db6d5f1a987d07dcc97ae
prakashid2 <prakashid2@gmail.com>
2023-03-07 22:19:16 +0530

    for running on tomcat

--- src/main/java/com/example/demo/EmployeeAppApplication.java
+++ src/main/java/com/example/demo/EmployeeAppApplication.java
@@ -2,9 +2,16 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class EmployeeAppApplication {
+public class EmployeeAppApplication extends SpringBootServletInitializer{
+
+	@Override
+	    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	        return application.sources(EmployeeAppApplication.class);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeAppApplication.class, args);
